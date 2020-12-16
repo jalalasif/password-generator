ff8a95dabe0b2af07897117f39f6352beba1ccc3
j-asif <jalal.asif97@gmail.com>
2020-12-15 21:57:38 -0400

    Commit 2: Made sure that duplicate elements can be added. Previously, choosing more than 10 in the 'numnber' choice category would cause the program to crash, since I was choosing by sample instead by random choice. This has been fixed, and now picks element with random iteration. Future updates will contain a GUI.

--- main.py
+++ main.py
@@ -3,27 +3,40 @@
 import random
 
 # Letters, numbers, and symbols to incorporate into the password
+import string
+
+# Letters, numbers and symbols to choose from, and a list to hold the assigned keys
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+pass_list = []
 
 # User prompts
 print("Welcome to the PyPassword Generator!")
+
+# Assign letters
 nr_letters = int(input("How many letters would you like in your password?\n"))
+for i in range(nr_letters):
+    r = random.choice(letters)
+    pass_list.append(r)
+
+# Assign symbols
 nr_symbols = int(input(f"How many symbols would you like?\n"))
+pass_symbols = ""
+for i in range(nr_symbols):
+    r = random.choice(symbols)
+    pass_list.append(r)
+
+# Assign numbers
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+pass_numbers = ""
+for i in range(nr_numbers):
+    r = random.choice(numbers)
+    pass_list.append(r)
 
-# Generates samples of random elements per user specifications
-rand_letters = random.sample(letters, nr_letters)
-rand_numbers = random.sample(numbers, nr_numbers)
-rand_symbols = random.sample(symbols, nr_symbols)
-
-# Combine into an ordered (unshuffled) password
-str_pass = rand_symbols + rand_letters + rand_symbols
-
-# Shuffle the password to break any predictability in structure and adjoin them into a string
-random.shuffle(str_pass)
-password = ''.join(str(elem) for elem in str_pass)
-print(password)
+# Combine into a shuffled password
+random.shuffle(pass_list)
+password = "".join(str(x) for x in pass_list)
+print(f"Your password is: {password}")
